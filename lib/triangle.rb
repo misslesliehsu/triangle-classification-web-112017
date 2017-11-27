1e1781435eb64d7a0d6e58d6ebc923127987e809
Flatiron School <leslie.hsu@flatironschool.com>
2017-11-27 08:08:27 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(num1, num2, num3)
+    @length_1 = num1
+    @length_2 = num2
+    @length_3 = num3
+  end
+
+  def kind
+    array = [@length_1, @length_2, @length_3]
+    sorted = array.sort
+    if array.include?(0)
+      begin
+        raise TriangleError
+      end
+    elsif sorted[2] >= (sorted[0] + sorted[1])
+      begin
+        raise TriangleError
+      end
+    elsif sorted[0] == sorted[1] && sorted[1] == sorted[2]
+      :equilateral
+    elsif sorted[0] == sorted[1] || sorted[1] == sorted[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
+
+  class TriangleError < StandardError
+    def message
+      "Nope!"
+    end
+  end
